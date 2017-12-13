12c02a2b32096205c9616e154cb11ef3d35cf487
Vishnu Singh <vishnu@localhost.localdomain>
2017-12-13 15:47:24 +0530

    allowed CORS

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ const api = require('./app/api');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Allow CORS
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 // api routes
 app.use('/', api);
 
