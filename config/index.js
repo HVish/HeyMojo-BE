3054e8e5a8030a6b1d6a9c8861148a63258ac08a
Vishnu Singh <vishnu@localhost.localdomain>
2017-12-14 14:43:54 +0530

    minor bug fixes

--- config/index.js
+++ config/index.js
@@ -1,19 +1,9 @@
 'use strict';
 
+const local = require('./local');
+const db = require('./db');
+
 module.exports = {
-    logLevel: 'debug', // error, warn, info, verbose, debug, silly
-    port: process.env.PORT || 3000,
-    env: process.env.NODE_ENV || 'development',
-    db: {
-        host: '',
-        port: 27017,
-        database: ''
-    },
-    aws: {
-        key: '',
-        secret: '',
-        region: '',
-        bucket: '',
-        uploadPath: ''
-    }
+    db,
+    local
 };
--- config/local.sample.js
+++ config/local.sample.js
@@ -1,7 +1,19 @@
 'use strict';
 
 module.exports = {
-    logLevel: 'info', // error, warn, info, verbose, debug, silly
+    logLevel: 'debug', // error, warn, info, verbose, debug, silly
     port: process.env.PORT || 3000,
-    env: process.env.NODE_ENV || 'development'
+    env: process.env.NODE_ENV || 'development',
+    db: {
+        host: '',
+        port: 27017,
+        database: ''
+    },
+    aws: {
+        key: '',
+        secret: '',
+        region: '',
+        bucket: '',
+        uploadPath: ''
+    }
 };
